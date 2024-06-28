efaf1955600b0736af4e94ed7dbcf9630c21c0f0
Jack OAT <jack.oat29@gmail.com>
2024-06-28 16:58:58 +0300

    Added logging to the index generator script

--- index_generator/index_generator.py
+++ index_generator/index_generator.py
@@ -23,19 +23,24 @@ VERBOSITY_LEVELS = [
 ]
 
 
-def error_exit(msg: str):
-    print(f"ERROR: {msg}")
-    exit(1)
+class Utils:
+    @staticmethod
+    def error_exit(msg: str):
+        logging.error(msg=msg)
+        exit(1)
 
-
-def remove_last_character(f_name: PathLike):
-    with open(f_name, "rb+") as f:
-        f.seek(-1, SEEK_END)
-        f.truncate()
+    @staticmethod
+    def remove_last_character(f_name: PathLike):
+        with open(f_name, "rb+") as f:
+            f.seek(-1, SEEK_END)
+            f.truncate()
 
 
 def process_repo(repo_url: str, lib_name: str) -> list:
+    logger = logging.getLogger(lib_name)
+    logger.debug("Creating temp dir")
     with tempfile.TemporaryDirectory() as repo_directory:
+        logger.info("Cloning the repo")
         run(
             ["git", "clone", "--depth", "1", repo_url, repo_directory],
             cwd=repo_directory,
@@ -44,6 +49,7 @@ def process_repo(repo_url: str, lib_name: str) -> list:
         )
 
         # Get all tags in library repo
+        logger.info("Fetching tags")
         run(
             ["git", "fetch", "--tags"],
             cwd=repo_directory,
@@ -51,6 +57,7 @@ def process_repo(repo_url: str, lib_name: str) -> list:
             check=True,
         )
 
+        logger.info("Getting the tag SHAs")
         proc = run(
             ["git", "show-ref", "--tags"],
             cwd=repo_directory,
@@ -62,10 +69,12 @@ def process_repo(repo_url: str, lib_name: str) -> list:
         tag_shas = [
             commit_tag.split()[0] for commit_tag in proc.stdout.strip().split("\n")
         ]
+        logger.debug(f"{tag_shas = }")
 
         # Parse manifest of every tag
         entries = []
         for tag_sha in tag_shas:
+            logger.info(f"Checking out SHA {tag_sha}")
             run(
                 ["git", "checkout", tag_sha],
                 cwd=repo_directory,
@@ -74,6 +83,7 @@ def process_repo(repo_url: str, lib_name: str) -> list:
             )
 
             # Get library info from manifest
+            logger.info("Parsing manifest")
             manifest_path = Path(repo_directory) / MANIFEST_FILENAME
             with open(manifest_path, "rb") as f:
                 manifest_data = tomllib.load(f)
@@ -82,14 +92,19 @@ def process_repo(repo_url: str, lib_name: str) -> list:
             # TODO: validate manifest here
 
             # Check name
+            logger.info("Checking name")
             if manifest_data["library"]["name"] != lib_name:
+                logging.warning(f"tag {tag_sha} in {repo_url} contains invalid name")
                 continue
                 # error_exit("mismatch")
 
             # Add to list
-            record = json.dumps(manifest_data["library"]) + ","
+            logging.info("Adding library to list")
+            record = json.dumps(manifest_data["library"]) + ", "
             entries.append(record)
 
+    logger.debug("entries:")
+    logger.debug(entries)
     return entries
 
 
@@ -151,19 +166,28 @@ def main():
         finally:
             l.release()
 
+    logging.debug("Adding first part to index file")
     with open(index_file_path, "w") as f_index:
         f_index.write('{"libraries":[')
 
+    logging.debug("Creating process pool")
+    logging.info("Opening accepted repositories file")
     with ppe(args.jobs) as p, open(accepted_repositories_path, "r") as f_arp:
+        logging.debug("Processing lines")
         while line := f_arp.readline():
             line = line.strip()
+            logging.debug(f"{line = }")
 
             if line.startswith("#"):
+                logging.debug("Skipping")
                 continue
 
             repo_url, lib_name = line.split(INDEX_SOURCE_SEPARATOR)
+            logging.debug(f"{repo_url = }")
+            logging.debug(f"{lib_name = }")
 
             # parse repo
+            logging.info(f"Processing repo: {repo_url}")
             future = p.submit(
                 process_repo,
                 repo_url=repo_url,
@@ -171,7 +195,7 @@ def main():
             )
             future.add_done_callback(lambda x: write_output(x.result()))
 
-    remove_last_character(index_file_path)
+    Utils.remove_last_character(index_file_path)
     write_output("]}")
 
 
