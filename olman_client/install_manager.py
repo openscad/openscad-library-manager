810716e32df7dd7dec44e5b64725d232d97e6d06
Jack OAT <jack.oat29@gmail.com>
2024-07-26 10:04:38 +0300

    Initial implementation for install and remove

--- olman_client/install_manager.py
+++ olman_client/install_manager.py
@@ -1,3 +1,5 @@
+from shutil import rmtree
+
 from olman import utils
 from olman.files.platform import getDataDir
 from olman.internal import local_index, remote_index
@@ -6,35 +8,44 @@ DOWNLOAD_LOCATION = getDataDir()
 INSTALL_LOCATION = getDataDir()
 
 
-def install(name: str, version_exact: str, force=False):
-    ridx = remote_index.RemoteIndex()
-    lidx = local_index.LocalIndex()
+def install(name: str, version_exact: str, *, force=False, reinstall=False):
+    # remote_index.update()
 
-    if local_matches := lidx.get(name):
-        for local_lib in local_matches:
-            if local_lib.manifest.library.version == version_exact:
-                # TODO: log "already installed"
+    if local_lib := local_index.get(name, default=None):
+        if utils.version_eq(local_lib.manifest.library.version, version_exact):
+            if reinstall:
+                remove(name)
+
+            else:
+                print(f"{name}:{version_exact} is already installed")
                 return
 
-            elif not force:
-                raise Exception("Another version is already installed")
+        elif force:
+            remove(name)
 
-    remote_matches = ridx.get(name, version_exact)
-    if len(remote_matches) != 1:
+        else:
+            raise Exception("Another version is already installed")
+
+    remote_lib = remote_index.get(
+        name, version_exact, default=None
+    )  # TODO: change 'get' to search?
+
+    if remote_lib is None:
         raise Exception(f"Could not find {name}:{version_exact} in the index")
 
-    remote_lib = remote_matches[0]
     compressed_lib_path = utils.downloadFile(
         remote_lib.download_link, dst=DOWNLOAD_LOCATION
     )
     lib_path = utils.extractFile(compressed_lib_path, dst_dir=INSTALL_LOCATION)
     lib_path = lib_path.rename(lib_path.with_name(name))
     compressed_lib_path.unlink()
-    lidx.add(remote_lib.manifest, lib_path)
+    local_index.add(remote_lib.manifest, lib_path)
 
 
-def remove(name: str):
-    pass
+def remove(name: str, missing_ok: bool = True):
+    if local_lib := local_index.get(name, default=None):
+        rmtree(local_lib.location)
+        local_index.remove(name)
 
-
-install("test_lib_1", "1.0.0")
+    elif not missing_ok:
+        raise Exception(f"Library {name} not found.")
